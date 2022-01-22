db23f8cde2a102296f4038d1b61d31090de7e797
Jake Christy <christyj@mail.gvsu.edu>
2022-01-22 15:49:31 -0500

    fixed errors when using tab controls (eg. next_tab()) with no active tabs

--- Main.gd
+++ Main.gd
@@ -43,33 +43,41 @@ func new_tab(container):
 # remove TextEdit node from tab container
 # ask to save changes before closing
 func close_tab(container):
-	var curr_tab = container.current_tab
-	var to_be_closed = container.get_child(curr_tab) # get the text editor to be closed
-	to_be_closed.queue_free() # remove text editor
-	# TODO: warn before closing if there is text
+	if tabs_exist(container):
+		var curr_tab = container.current_tab
+		var to_be_closed = container.get_child(curr_tab) # get the text editor to be closed
+		to_be_closed.queue_free() # remove text editor
+		# TODO: warn before closing if there is text
 
 # cycle active TextEdit to the right
 func next_tab(container):
-	var curr_tab = container.current_tab
-	var last_tab = container.get_tab_count() - 1
-	
-	if curr_tab == last_tab: # check for wrapping
-		container.current_tab = 0
-	else:
-		container.current_tab = curr_tab + 1 # move right
+	if tabs_exist(container):
+		var curr_tab = container.current_tab
+		var last_tab = container.get_tab_count() - 1
+		
+		if curr_tab == last_tab: # check for wrapping
+			container.current_tab = 0
+		else:
+			container.current_tab = curr_tab + 1 # move right
 
 # cycle active TextEdit to the left
 func prev_tab(container):
-	var first_tab = 0
-	var curr_tab = container.current_tab
-	var last_tab = container.get_tab_count() -1
-	
-	if curr_tab ==  first_tab: # check for wrapping
-		container.current_tab = last_tab
-	else:
-		container.current_tab = curr_tab - 1 # move left
+	if tabs_exist(container):
+		var first_tab = 0
+		var curr_tab = container.current_tab
+		var last_tab = container.get_tab_count() -1
+		
+		if curr_tab ==  first_tab: # check for wrapping
+			container.current_tab = last_tab
+		else:
+			container.current_tab = curr_tab - 1 # move left
 
-	
+func tabs_exist(container):
+	if container.get_tab_count() > 0:
+		return true
+	else:
+		return false 
+
 # File control functions
 # show a file dialog, then put text into current TextEdit
 func open_file():
